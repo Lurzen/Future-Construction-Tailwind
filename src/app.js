549b460168635438986b6324e276a81147fb676d
Ali Ali <ali.yksl7@gmail.com>
2026-02-02 13:07:05 +0300

    update

--- src/app.js
+++ src/app.js
@@ -5,69 +5,153 @@ document.addEventListener("DOMContentLoaded", function () {
   };
 });
 
-// Projects Slider
-document.addEventListener("DOMContentLoaded", function () {
-  var main = new Splide("#main-slider", {
-    type: "fade",
-    pagination: false,
-    arrows: false,
-    drag: false,
-  });
+// Language Select
+document.addEventListener("click", (e) => {
+  const lang = document.getElementById("lang-dropdown");
+  if (!lang) return;
 
-  var text = new Splide("#text-slider", {
-    type: "fade",
-    pagination: false,
-    arrows: false,
-    drag: false,
-  });
-
-  var thumbnails = new Splide("#thumbnail-slider", {
-    fixedWidth: true,
-    perPage: 1.5,
-    gap: 30,
-    pagination: false,
-    arrows: false,
-    isNavigation: true,
-    focus: 0,
-  });
-
-  main.sync(text);
-  main.sync(thumbnails);
-
-  main.mount();
-  text.mount();
-  thumbnails.mount();
-
-  var nextBtn = document.querySelector(".js-next");
-  var prevBtn = document.querySelector(".js-prev");
-
-  if (nextBtn && prevBtn) {
-    nextBtn.addEventListener("click", () => main.go(">"));
-    prevBtn.addEventListener("click", () => main.go("<"));
+  if (lang.contains(e.target)) {
+    lang.classList.toggle("active");
+  } else {
+    lang.classList.remove("active");
   }
+});
+
+// Hero Slider
+document.addEventListener("DOMContentLoaded", function () {
+  var heroSwiper = new Swiper(".heroSlider", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    navigation: {
+      nextEl: ".hero-nextbtn",
+      prevEl: ".hero-prevbtn",
+    },
+  });
+  var textSwiper = new Swiper(".hero-textSlider", {
+    loop: true,
+    effect: "creative",
+    creativeEffect: {
+      prev: {
+        translate: [0, 0, -400],
+      },
+      next: {
+        translate: ["100%", 0, 0],
+      },
+    },
+    allowTouchMove: false,
+  });
+  heroSwiper.controller.control = textSwiper;
+});
+
+// Scroll Down
+document.querySelector(".scroll-down").addEventListener("click", function (e) {
+  gsap.registerPlugin(ScrollToPlugin);
+  const target = this.getAttribute("href");
+  e.preventDefault();
+  gsap.to(window, {
+    duration: 1.8,
+    scrollTo: target,
+    ease: "power1.inOut",
+  });
 });
 
 // News Slider
 document.addEventListener("DOMContentLoaded", function () {
-  var newsSplide = new Splide("#news-slider", {
-    type: "loop",
-    perPage: 2.5,
-    perMove: 1,
-    gap: "4rem",
-    padding: "2rem",
+  var newsSwiper = new Swiper("#news-slider", {
+    speed: 1000,
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loopedSlides: 5,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+      dynamicMainBullets: 1,
+    },
+    navigation: {
+      nextEl: ".news-nextBtn",
+      prevEl: ".news-prevBtn",
+    },
 
     breakpoints: {
       1024: {
-        perPage: 1,
+        slidesPerView: 2.5,
+        spaceBetween: 64,
       },
     },
-    focus: 0,
-    omitEnd: true,
   });
-  newsSplide.mount();
+});
+
+// Projects Slider
+document.addEventListener("DOMContentLoaded", function () {
+  var mainSwiper = new Swiper("#main-slider", {
+    effect: "fade",
+    fadeEffect: { crossFade: true },
+    allowTouchMove: false,
+    navigation: {
+      nextEl: ".js-next",
+      prevEl: ".js-prev",
+    },
+  });
+
+  var textSwiper = new Swiper("#text-slider", {
+    effect: "fade",
+    allowTouchMove: false,
+  });
+
+  var thumbnailSwiper = new Swiper("#thumbnail-slider", {
+    slidesPerView: 1.5,
+    spaceBetween: 30,
+  });
+
+  function screenController() {
+    if (window.innerWidth >= 1024) {
+      mainSwiper.controller.control = [textSwiper, thumbnailSwiper];
+      thumbnailSwiper.controller.control = [mainSwiper, textSwiper];
+    } else {
+      mainSwiper.controller.control = textSwiper;
+      thumbnailSwiper.controller.control = undefined;
+    }
+  }
+  screenController();
+  window.addEventListener("resize", screenController);
 });
 
 // Iso Animation
 AOS.init({
   once: true,
+});
+
+// Footer Accordion
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelectorAll(".accordion-btn").forEach((btn) => {
+    btn.addEventListener("click", () => {
+      btn.classList.toggle("active");
+      btn.nextElementSibling.classList.toggle("active");
+    });
+  });
+});
+
+// Footer Office Slider
+document.addEventListener("DOMContentLoaded", function () {
+  var footerOfficeSwiper = new Swiper(".footer-office-swiper", {
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 30,
+    navigation: {
+      nextEl: ".footer-office-next",
+      prevEl: ".footer-office-prev",
+    },
+    breakpoints: {
+      768: {
+        spaceBetween: 0,
+        allowTouchMove: false,
+      },
+      1025: {
+        slidesPerView: 4,
+        allowTouchMove: false,
+      },
+    },
+  });
 });
